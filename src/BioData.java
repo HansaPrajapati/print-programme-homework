9d98a3dcb120b2a5e7957f67097f684144d47c61
HansaPrajapati <prajhansa29@gmail.com>
2023-10-09 16:57:13 +0100

    homework

---
+++ src/BioData.java
@@ -0,0 +1,15 @@
+public class BioData {
+    public static void main(String [] args){
+        System.out.println("Name : Yes");
+        System.out.println("E-mail : yes.29@gmail.com");
+        System.out.println("Age : 25");
+        System.out.println("Contact Number: 0782453671");
+        System.out.println("Qualifications: Software Engineer");
+        System.out.println(" address: India");
+        System.out.println("Date of Birth :10-06-2000");
+        System.out.println("Religion : Hindu");
+
+
+
+    }
+}
---
+++ src/input.java
@@ -0,0 +1,7 @@
+public class input {
+    public static void main(String[] args){
+        int a = 25;
+        int b = 5;
+        System.out.println(a*b);
+    }
+}
